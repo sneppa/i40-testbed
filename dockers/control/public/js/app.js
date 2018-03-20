c940a42b9ea32cbd5a4ae4fbb287cc86d62a0508
weber <1414353@stud.hs-mannheim.de>
2018-03-20 17:24:26 +0100

    finished "Producttype add"

--- dockers/control/public/js/app.js
+++ dockers/control/public/js/app.js
@@ -2,86 +2,129 @@
 var app = angular.module('App', ['ngRoute']);
 
 //create an app router for url management and redirect
-app.config(function($routeProvider) {
-	$routeProvider.when('/dashboard', {
-		templateUrl: 'tpl/dashboard.html',
-		controller: 'dashboard',
-	}).when('/server', {
-		templateUrl: 'tpl/server.html',
-		controller: 'server',
-	}).when('/products', {
-		templateUrl: 'tpl/products.html',
-		controller: 'products',
-	}).when('/products/produce', {
-		templateUrl: 'tpl/products/produce.html',
-		controller: 'products',
-	}).when('/products/add', {
-		templateUrl: 'tpl/products/add.html',
-		controller: 'products',
-	}).when('/products/manage', {
-		templateUrl: 'tpl/products/manage.html',
-		controller: 'products',
-	}).when('/network', {
-		templateUrl: 'tpl/network.html',
-		controller: 'network',
-	}).when('/help', {
-		templateUrl: 'tpl/help.html',
-	});
-	$routeProvider.otherwise({redirectTo: '/dashboard'});
+app.config(function ($routeProvider) {
+    $routeProvider.when('/dashboard', {
+        templateUrl: 'tpl/dashboard.html',
+        controller: 'dashboard',
+    }).when('/server', {
+        templateUrl: 'tpl/server.html',
+        controller: 'server',
+    }).when('/products', {
+        templateUrl: 'tpl/products.html',
+        controller: 'products',
+    }).when('/products/produce', {
+        templateUrl: 'tpl/products/produce.html',
+        controller: 'products',
+    }).when('/products/add', {
+        templateUrl: 'tpl/products/add.html',
+        controller: 'products',
+    }).when('/products/manage', {
+        templateUrl: 'tpl/products/manage.html',
+        controller: 'products',
+    }).when('/network', {
+        templateUrl: 'tpl/network.html',
+        controller: 'network',
+    }).when('/help', {
+        templateUrl: 'tpl/help.html',
+    });
+    $routeProvider.otherwise({redirectTo: '/dashboard'});
 });
 
-app.controller('nav', function($scope) {
+app.controller('nav', function ($scope) {
 //    $scope.title = "Dashboard";
 });
 
-app.controller('dashboard', function($scope) {
+app.controller('dashboard', function ($scope) {
 //    $scope.nav.title = "Dashboard";
 });
 
-app.controller('server', function($scope) {
+app.controller('server', function ($scope) {
 //    $scope.nav.title = "Server";
-    
+
 });
 
-app.controller('products', function($scope) {
+app.controller('products', function ($scope) {
 //    $scope.nav.title = "Produkte";
-    
+
 });
 
-app.controller('network', function($scope) {
+app.controller('network', function ($scope) {
 //    $scope.nav.title = "Netzwerk";
-    
+
 });
 
+// ------------------------------- Products
 
-app.controller('ProductAdd', function ($scope, $templateCache) {
-    
+app.controller('ProductManage', function ($scope, $http) {
+
+});
+
+app.controller('ProductAdd', function ($scope, $http) {
+
     var productart = {};
     productart.name = "";
     productart.step = [];
     productart.var = [];
     $scope.productart = productart;
-    
+
     var unitFields = 0;
     $scope.productart.step = [];
     $scope.productart.step[0] = {"index": 0, "name": ""};
-    
+
     $scope.addUnit = function () {
         unitFields++;
         $scope.productart.step[unitFields] = {"index": unitFields, "name": ""};
     }
-    
+
     var varFields = 0;
     $scope.productart.var = [];
     $scope.productart.var[0] = {"index": 0, "name": "", "type": "String"};
-    
+
     $scope.addVar = function () {
         varFields++;
         $scope.productart.var[varFields] = {"index": varFields, "name": "", "type": "String"};
     }
-    
+
     $scope.saveForm = function () {
         console.log($scope.productart);
+
+        $http.post('/api/producttype', $scope.productart).then(
+                function () { console.log("Gespeichert"); showSuccess("Produkttyp \""+$scope.productart.name+"\" gespeichert!"); }, 
+                function () { console.log("Fehler"); showStandardError(); });
     }
-    
+
 });
+
+
+
+
+
+// ------------------------------- Notifications
+
+function showSuccess(text)
+{
+    showNotification("check", "success", text);
+}
+function showError(text)
+{
+    showNotification("close-circle", "danger", text);
+}
+function showStandardError()
+{
+    showNotification("close-circle", "danger", "Fehler aufgetreten!");
+}
+
+function showNotification(icon, type, text){
+	$.notify({
+		icon: "pe-7s-"+icon,
+		message: text
+
+	},{
+		type: type,
+		timer: 4000,
+		placement: {
+			from: "bottom",
+			align: "right"
+		}
+	});
+}
--- dockers/control/server.js
+++ dockers/control/server.js
@@ -1,19 +1,69 @@
 'use strict';
 
 var express = require("express");
-var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 var app = express();
+var logger;
 
+
+var url = 'mongodb://localhost:27017';
+var dbName = 'interface';
+
+// Bereitstellen des static contents
 app.use(express.static('public'));
+// BodyParsergeschiss
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/interface');
-
-app.post('/api/producttype', function (req, res) {
-    
-    console.log(req);
-    
-});
-
+// App bereitstellen
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+    logger('Example app listening on port 8080!');
 });
+
+
+// Datenbankoperationen
+MongoClient.connect(url, function (err, client) {
+    assert.equal(null, err);
+    logger("Connected successfully to mongoDB server");
+    var db = client.db(dbName);
+
+    app.post('/api/producttype', function (req, res) {
+
+        var collection = db.collection('producttypes');
+
+        collection.insert(req.body, function (err, records) {
+
+            if (err == null)
+                res.status(200);
+            else
+                res.status(500);
+
+            res.send("");
+        });
+        logger(req.body);
+    });
+    app.get('/api/producttype', function (req, res) {
+
+        var collection = db.collection('producttypes');
+
+        collection.find({}).toArray(function (err, result) {
+            if (err == null)
+            {
+                res.status(200);
+                res.send(result);
+            } else
+            {
+                res.status(500);
+                res.send("");
+            }
+            console.log(result);
+        });
+    });
+
+});
+
+logger = function (text) {
+    console.log(text);
+};
