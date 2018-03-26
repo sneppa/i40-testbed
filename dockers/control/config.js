90377e87a0d042869d531ae14aac4c5fb62f6e6b
weber <1414353@stud.hs-mannheim.de>
2018-03-26 11:34:42 +0200

    changed docker config

--- dockers/control/config.js
+++ dockers/control/config.js
@@ -5,12 +5,12 @@ config.port = 8080; // Port des API Servers
 // MongoDB Einstellungen
 config.db = {};
 //config.db.url = 'mongodb://localhost:27017'; // Started with: node
-config.db.url = 'mongodb://mongodb'; // Started with: docker
+config.db.url = 'mongodb://controldb:27017'; // Started with: docker
 config.db.name = 'interface';
 
 // URL zur Repository API
-config.repository = 'http://localhost:8090/api'; // Started with: node
-//config.repository = 'http://localhost:8090/api'; // Started with: docker
+//config.repository = 'http://localhost:8090/api'; // Started with: node
+config.repository = 'http://repository:8090/api'; // Started with: docker
 //config.repository = 'http://localhost:8090/api'; // Started with: kubernetes
 
 config.discovery = {};
