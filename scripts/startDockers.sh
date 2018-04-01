f85275f029274ec9a18e9c002bd07eb97d27b8c9
Martin Weber <sneppa@gmx.de>
2018-04-01 20:19:15 +0200

    added Scheduler to start script

--- scripts/startDockers.sh
+++ scripts/startDockers.sh
@@ -5,3 +5,5 @@ sudo docker-compose --file ./dockers/discovery/docker-compose.yml up -d
 sudo docker-compose --file ./dockers/repository/docker-compose.yml up -d
 
 sudo docker-compose --file ./dockers/control/docker-compose.yml up -d
+
+sudo docker-compose --file ./dockers/scheduler/docker-compose.yml up -d
