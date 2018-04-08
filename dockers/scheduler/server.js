6b7432f2931958d7c74132de9d5c30c600a2177f
Martin Weber <sneppa@gmx.de>
2018-04-09 00:07:32 +0200

    removed unnecessary code

--- dockers/scheduler/server.js
+++ dockers/scheduler/server.js
@@ -8,22 +8,6 @@ var config = require('./config');
 var startTime = new Date();
 var timer = null;
 
-// scheduler.getServers(true, function (err) {
-
-//     if (err) console.error(err.message);
-//     // configs is now a map of JSON data
-//     console.log("Fertig initialisiert");
-//     scheduler.printServers();
-
-//     var endTime = new Date();
-
-//     var timeDiff = (endTime - startTime) / 1000;
-//     console.log("Init Time: "+timeDiff);
-
-//     setCrawlInterval();
-
-// });
-
 setCrawlInterval();
 
 function setCrawlInterval() { 
